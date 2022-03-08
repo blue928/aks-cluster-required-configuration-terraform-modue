71deed952413c03bddcc577fef7a0e159a398610
Blue Presley <bluepresley@gastonia.com>
2022-03-08 09:38:04 -0500

    initial commit

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
 
+# ---------------------------------------------------------------------------------------------------------------------
+# AKS CLUSTER REQUIRED CONFIGURATION
+# After the AKS Cluster is created this default configuration must be applied.
+# The resources that are created are:
+#  - Storage:
+#    - storage-aks-azure-file-share-storage-class.tf 
+#  - Namespace
+#    - namespace-aks-cluster-production-ns.tf 
+# todo: add the others
+# ---------------------------------------------------------------------------------------------------------------------
