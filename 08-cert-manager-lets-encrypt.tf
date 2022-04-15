50f3de060bf652dfb511788d4da4bb1ccc081ae1
Blue Presley <bluepresley@gastonia.com>
2022-04-15 09:03:55 -0400

    update

--- 08-cert-manager-lets-encrypt.tf
+++ 08-cert-manager-lets-encrypt.tf
@@ -32,7 +32,7 @@ resource "helm_release" "cert-manager" {
 # for a better solution that doesn't require a 2nd apply.
 
 # TODO - add this to its own file?
-resource "kubernetes_manifest" "clusterissuer_letsencrypt_prod" {
+/*resource "kubernetes_manifest" "clusterissuer_letsencrypt_prod" {
   manifest = {
     "apiVersion" = "cert-manager.io/v1"
     "kind"       = "ClusterIssuer"
