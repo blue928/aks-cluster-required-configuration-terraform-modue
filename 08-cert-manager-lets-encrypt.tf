5a19841abd718bc367443d4f8df3f2971b6f8d69
Blue Presley <bluepresley@gastonia.com>
2022-04-15 19:46:52 -0400

    update

--- 08-cert-manager-lets-encrypt.tf
+++ 08-cert-manager-lets-encrypt.tf
@@ -1,4 +1,4 @@
-resource "helm_release" "cert-manager" {
+/*resource "helm_release" "cert-manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
@@ -32,7 +32,7 @@ resource "helm_release" "cert-manager" {
 # for a better solution that doesn't require a 2nd apply.
 
 # TODO - add this to its own file?
-resource "kubernetes_manifest" "clusterissuer_letsencrypt_prod" {
+/*resource "kubernetes_manifest" "clusterissuer_letsencrypt_prod" {
   manifest = {
     "apiVersion" = "cert-manager.io/v1"
     "kind"       = "ClusterIssuer"
--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@
 terraform {
   required_providers {
     kubectl = {
-      source  = "gavinbunney/kubectl"
+      source = "gavinbunney/kubectl"
       #version = ">= 1.7.0"
     }
   }
