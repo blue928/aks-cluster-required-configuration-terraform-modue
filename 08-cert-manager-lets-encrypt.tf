a3eddb81df6b434ea2ba55c76389aa87dba0789d
Blue Presley <bluepresley@gastonia.com>
2022-04-18 14:12:31 -0400

    update

--- 08-cert-manager-lets-encrypt.tf
+++ 08-cert-manager-lets-encrypt.tf
@@ -1,4 +1,4 @@
-/*resource "helm_release" "cert-manager" {
+resource "helm_release" "cert-manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
